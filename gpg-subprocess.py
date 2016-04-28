dbfd004179a4fbca2c2f99988d23e791fc4f7160
Herman Slatman <hermanslatman@hotmail.com>
2016-04-28 12:37:11 +0200

    Running GPG from Python via subprocess

---
+++ gpg-subprocess.py
@@ -0,0 +1,5 @@
+#https://stackoverflow.com/questions/13332268/python-subprocess-command-with-pipe
+
+sp = subprocess.Popen(("echo", "<some_password>"), stdout=subprocess.PIPE)
+output1 = subprocess.check_output(("gpg", "--batch", "--homedir", "<location_of_home_directory/.gnupg>", "--no-tty", "--yes", "--passphrase-fd", "0", "--symmetric", "-o", "<output_file>", "<input_file>"), stdin=sp.stdout)
+sp.wait()
