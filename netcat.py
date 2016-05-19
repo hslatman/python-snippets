2e1c4cc5ed34277abd56f707171364e3fb915160
Herman Slatman <hermanslatman@hotmail.com>
2016-05-19 12:29:36 +0200

    Smal netcat-like class

---
+++ netcat.py
@@ -0,0 +1,46 @@
+import socket
+
+class Netcat:
+
+    """ Python 'netcat like' module """
+
+    def __init__(self, ip, port):
+
+        self.buff = ""
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((ip, port))
+
+    def read(self, length = 4096):
+
+        """ Read 1024 bytes off the socket """
+
+        return self.socket.recv(length)
+ 
+    def read_until(self, data):
+
+        """ Read data into the buffer until we have data """
+
+        while not data in self.buff:
+            self.buff += self.socket.recv(4096)
+ 
+        pos = self.buff.find(data)
+        rval = self.buff[:pos + len(data)]
+        self.buff = self.buff[pos + len(data):]
+ 
+        return rval
+ 
+    def write(self, data):
+
+        self.socket.send(data)
+    
+    def close(self):
+
+        self.socket.close()
+        
+        
+        
+# usage:
+# from netcat import Netcat
+# nc = Netcat('<ip_address>', <port>)
+# nc.read()
+# nc.write(<something>)
